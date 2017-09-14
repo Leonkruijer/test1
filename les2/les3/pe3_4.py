73e16bdf81aecef72ba6db517c6c6f2c2e9fe69d
leonkrujer <leon.kruijer@student.hu.nl>
2017-09-14 12:40:27 +0200

    Les 3

---
+++ les2/les3/pe3_4.py
@@ -0,0 +1,4 @@
+lijst = [ 'maandag', 'dinsdag', 'woensdag' ]
+
+for word in lijst  :
+        print (word[0]+word[1])
---
+++ les2/les3/pe3_5.py
@@ -0,0 +1,3 @@
+lijst= [0,1,2,3,4,5,6,7,8,9,10]
+for lijst in range(0, 10, 2) :
+    print (lijst)
